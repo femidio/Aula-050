8d5f43c962f8db7f519784b8794f7b94a172bd77
femidio <88860188+femidio@users.noreply.github.com>
2024-03-12 13:47:43 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from markupsafe import Markup
+import socket
+import os
+from werkzeug.user_agent import UserAgent
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -13,7 +17,11 @@ moment = Moment(app)
 
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+    name = StringField('What is your first name?', validators=[DataRequired()])
+    sobrenome = StringField('What is your last name?', validators=[DataRequired()])
+    prontuario = StringField('What is your student ID?', validators=[DataRequired()])
+    instituicao = StringField('What is your institution?', validators=[DataRequired()])
+    discipline = StringField('What is your discipline name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -35,5 +43,11 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        session['sobrenome'] = form.sobrenome.data
+        session['prontuario'] = form.prontuario.data
+        session['instituicao'] = form.instituicao.data
+        session['discipline'] = form.discipline.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    user_agent = UserAgent(request.headers.get('User-Agent'))
+    return render_template('index.html', form=form, name=session.get('name'), sobrenome=session.get('sobrenome'), prontuario=session.get('prontuario'), instituicao=session.get('instituicao'), discipline=session.get('discipline'), ip=ip, user_agent=user_agent)
